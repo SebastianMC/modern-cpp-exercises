8a6d3b55f4c6d3a6019a5f27000cc5673907a337
SebastianMC <23032356+SebastianMC@users.noreply.github.com>
2023-11-21 23:16:16 +0100

    Initial Commit

---
+++ modern-cpp-exercises/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  modern-cpp-exercises
+//
+//  Created by Sebastian on 21/11/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
